d7b453cb27d2339e29f3143aa9740bfa79af774b
Dhanraj <dp914278@gmail.com>
2025-07-09 21:16:28 +0530

    Fixed Dependencies issue

--- example/main.dart
+++ example/main.dart
@@ -1,13 +1,14 @@
 import 'package:radarr/radarr.dart';
 
 void main() async {
-    // The host must include the protocol
-    // If required, the host should include the port and the base URL as well
-    String host = 'http://192.168.1.111:7878';
-    // Your key can be fetched from the Radarr web GUI
-    String key = '<apikey>';
-    Radarr api = Radarr(host: host, apiKey: key);
-    // Run your commands
-    // Example to get and print the title of the movie with identifier 1
-    api.movie.get(movieId: 1).then((data) => print(data.title));
+  // The host must include the protocol
+  // If required, the host should include the port and the base URL as well
+  String host = '';
+  // Your key can be fetched from the Radarr web GUI
+  String key = '';
+  Radarr api = Radarr(host: host, apiKey: key);
+  // Run your commands
+  // Example to get and print the title of the movie with identifier 1
+  // api.movie.get(movieId: 1).then((data) => print(data.title));
+  api.movie.getAll().then((data) => print(data.first.images!.first.url));
 }
